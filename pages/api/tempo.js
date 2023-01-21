1afb58519e6d1ad945088ff3fe348c4dfa1340d1
apolo14 <felipe_cenarios@hotmail.com>
2023-01-20 21:11:28 -0300

    Update tempo.js

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,13 @@
-function tempo(req, response){
+async function tempo(req, response){
     const dynamicDate = new Date();
 
+    const subscribersResponse = await fetch(125612351531351531531531351518);
+    const subscribersResponseJson = await subscribersResponse.json();
+    const inscritos = subscribersResponseJson.total_subscribers;
+
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        inscritos: inscritos
     })
 }
 
